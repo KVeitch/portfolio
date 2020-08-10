58c650b8403fd8a6ef97f12175fc22c5baa90ca1
KVeitch <kirk.a.veitch@gmail.com>
2020-08-10 14:31:02 -0600

    Format and link Footer

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
-import React, {useState, useEffect} from 'react';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import {Container, Navbar, Nav} from 'react-bootstrap'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
-  const headerData =[
-    { title: 'Home', path: '/' },
-    { title: 'About', path: '/about' },
-    { title: 'Contact', path: '/contact' }
-  ]
+  const headerData = [
+    { title: "Home", path: "/" },
+    { title: "About", path: "/about" },
+    { title: "Contact", path: "/contact" },
+  ];
   const homeData = {
-    title: 'Keep Moving Forward',
-    subTitle: 'Projects, and add sub title',
-    text: 'Take a look at my projects below'
-  }
+    title: "Keep Moving Forward",
+    subTitle: "Projects, and add sub title",
+    text: "Take a look at my projects below",
+  };
 
   const [title, setTitle] = useState("Kirk Veitch");
   const [headerLinks, setHeaderLinks] = useState(headerData);
   const [home, setHome] = useState(homeData);
-  const [about, setabout] = useState({title: 'About Me'});
-  const [contact, setContact] = useState({title: 'Get in touch'});
+  const [about, setabout] = useState({ title: "About Me" });
+  const [contact, setContact] = useState({ title: "Get in touch" });
 
   return (
     <Router>
-        <Container fluid={true} className='p-0'>
-          <Navbar className="border-bottom" bg='transparent' expand='lg'>
-            <Navbar.Brand>Kirk Veitch</Navbar.Brand>
-            <Navbar.Toggle aria-controls='navbar-toggle' className="border-0"/>
-            <Navbar.Collapse id="navbar-toggle">
-              <Nav className="ml-auto">
-                <Link className="nav-link mr-auto" to='/'>Home</Link> 
-                <Link className="nav-link" to='/about'>About</Link> 
-                <Link className="nav-link" to='/contact'>Contact</Link> 
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          
-        </Container>
+      <Container fluid={true} className="p-0">
+        <Navbar className="border-bottom" bg="transparent" expand="lg">
+          <Navbar.Brand>Kirk Veitch</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-toggle" className="border-0" />
+          <Navbar.Collapse id="navbar-toggle">
+            <Nav className="ml-auto">
+              <Link className="nav-link mr-auto" to="/">Home</Link>
+              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link" to="/contact">Contact</Link>
+              <Container className='justify-content-start ml-auto p-0 m-0'>
+                <Link className="nav-link" to="www.linkedin.com">LinkedIn.ico</Link>
+                <Link className="nav-link" to="www.linkedin.com">GitHub.ico</Link>
+              </Container>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Footer />
+      </Container>
     </Router>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import {Container, Row, Col} from 'react-bootstrap'
 const Footer = () => {
     return (
-        <div>
+        <footer className='mt-5'>
+            <Container fluid={true}>
+                <Row className='border-top justify-content-between p-3' >
+                    <Col className='p-0' sd={12} md={3}>Kirk Veitch</Col>
+                    <Col className='p-0 d-flex justify-content-end' md={3}>Crafted by Kirk Veitch</Col>
+                </Row>
+            </Container>
             
-        </div>
+        </footer>
     )
 }
 
