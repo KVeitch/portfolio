a52c5c1d203bbf61cb10befc50cf16cac8ab5b4e
KVeitch <kirk.a.veitch@gmail.com>
2020-08-11 00:45:18 -0600

    Implement Card and CardInfo components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Card = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import CardInfo from "./CardInfo"
+
+const Card = ({item, click}) => {
+  return(
+    <div className="d-inline-block p-card" onClick={(e) => click(item)}>
+      <img className="p-card-image" src={item.imgSrc} alt={item.imgSrc} />
+      { item.selected && <CardInfo title={item.title} subTitle={item.subTitle} link={item.link} /> }
+    </div>
+  );
 }
 
 export default Card
--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useSpring, animated } from 'react-spring';
 
-const CardInfo = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const CardInfo = ({title, subTitle, link}) => {
+  const style = useSpring({opacity: 1, from: {opacity: 0}});
 
-export default CardInfo
+  return (
+    <animated.div className="p-card-info" style={style}>
+      <p className="p-card-title">{title}</p>
+      <p className="p-card-sub-title">{subTitle}</p>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        View
+      </a>
+    </animated.div>
+  );
+};
+
+export default CardInfo;
